f0bab721d7dd5c42450123afd3642a4fee8729c8
DELL <DELL@DESKTOP-D8T6JFS>
2021-06-28 08:07:28 +0700

    Add project files.

---
+++ lab4-5-6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab4_5_6.MvcApplication" Language="C#" %>
---
+++ lab4-5-6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab4_5_6.Startup))]
+namespace lab4_5_6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
